0ae7e219b47dd552092e0be4336558c902fbcbf5
John Buley <johnmbuley@gmail.com>
2015-08-19 13:17:48 -0500

    IdfWord was not added previously

---
+++ src/main/java/idfWord.java
@@ -0,0 +1,10 @@
+
+public class idfWord {
+    public final int index;
+    public final double idf;
+
+    public idfWord(int index, double idf) {
+        this.index = index;
+        this.idf = idf;
+    }
+}
